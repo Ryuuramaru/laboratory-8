c760496beeccb746cb86d6c23bf02641a4b581e8
Matei Rosu <matei.rosu@stud.ubbcluj.ro>
2022-04-12 17:25:42 +0300

    copied everything into h

--- Laboratory 8/DynamicArray.h
+++ Laboratory 8/DynamicArray.h
@@ -4,22 +4,83 @@
 #include "Offer.h"
 using namespace std;
 
-typedef Offer ElementType;
+// typedef Offer ElementType;
+
+template <typename ElementType>
 
 class DynamicArray
 {
 public:
-	DynamicArray(int capacity = 100); // = default parameter
+	DynamicArray(int capacity = 100) // = default parameter
+	{
+		length = 0;
+		this->capacity = capacity;
+		data = new ElementType[capacity]();
+
+		if (data == nullptr)
+		{
+			cerr << "DynamicArray(): failed to allocate memory" << endl;
+			exit(-1);
+		}
+	}
 
 	// RULE OF THREE 
 	// destructor
-	~DynamicArray();
+	~DynamicArray()
+	{
+		// free the allocated memory
+		delete[] data;
+	}
 
 	// copy constructor
-	DynamicArray(const DynamicArray& other);
+	DynamicArray(const DynamicArray &other)
+	{
+		// deep copy of other
+		length = other.length;
+		capacity = other.capacity;
+
+		// !! data
+		// data = other.data; NOO! this is shallow copy
+		// deep copy of data
+		data = new ElementType[other.capacity]();
+
+		if (data == nullptr)
+		{
+			cerr << "DynamicArray(const DynamicArray& other): failed to allocate memory" << endl;
+			exit(-1);
+		}
+
+		for (unsigned int i = 0; i < length; i++)
+			data[i] = other.data[i];
+	}
 
 	// assignment operator
-	DynamicArray& operator=(const DynamicArray& other);
+	DynamicArray &operator=(const DynamicArray &other)
+	{
+
+		if (this != &other)
+		{ // self assignment check
+			// deep copy of other
+			length = other.length;
+			capacity = other.capacity;
+
+			delete[] this->data;
+			// !! data
+			// data = other.data; NOO! this is shallow copy
+			// deep copy of data
+			data = new ElementType[other.capacity]();
+
+			if (data == nullptr)
+			{
+				cerr << "operator=(): failed to allocate memory" << endl;
+				exit(-1);
+			}
+
+			for (unsigned int i = 0; i < length; i++)
+				data[i] = other.data[i];
+		}
+		return *this; // the type of this is DynamicArray* -> dereferencing
+	}
 	// end RULE OF THREE 
 
 	// getter for the length
@@ -29,24 +90,94 @@ public:
 	inline unsigned int getCapacity() const { return capacity; }
 
 	// adds an element at the end
-	void append(ElementType v, bool* err = nullptr);
+	void append(ElementType v, bool *err = nullptr)
+	{
+		// todo
+		// check if the length < capacity
+		if (length == capacity)
+		{
+			// double the capacity
+			resize(capacity * 2);
+		}
+		data[length] = v;
+		length++;
+	}
 
 	// remove the element from the end
 	// pre: the array is not empty
-	ElementType popBack();
+	ElementType popBack()
+	{
+		// delete and return the last element in the array
+		// pre array is not empty
+		if (length == 0)
+		{
+			// unfriendly way of handling corner cases
+			cerr << "Precondition does not hold: popBack()" << endl;
+			exit(-1);
+		}
+
+		// convention 0 - elements that were not set
+
+		ElementType v = data[length];
+		length--;
+
+		return v;
+	}
 
 	// remove an element from position i
 	// index >= 0 and index < length
-	ElementType remove(unsigned int index);
+	ElementType remove(unsigned int index)
+	{
+		//  index >= 0 and index < length
+		// pre index >=0 and index < length
+		if (index >= 0 && index < length)
+		{
+			ElementType v = data[index];
+			for (unsigned int i = index; i < length - 1; i++)
+				data[i] = data[i + 1];
+			length--;
+			return v;
+		}
+		else
+		{
+			// unfriendly way of handling corner cases
+			cerr << "Precondition does not hold: remove()" << endl;
+			exit(-1);
+		}
+	}
 
 	// get the element on a position
 	// a[10]
 	// index >= 0 and index < length
 	// -1 is returned if the index is not valid
-	ElementType& get(unsigned int index);
+	ElementType &get(unsigned int index)
+	{
+
+		// pre index >=0 and index < length
+		if (index >= 0 && index < length)
+		{
+			return data[index];
+		}
+		else
+		{
+			// unfriendly way of handling corner cases
+			cerr << "Precondition does not hold: get()" << endl;
+			exit(-1);
+		}
+	}
 
 	// print the array
-	friend ostream& operator<<(ostream& s, const DynamicArray& arr);
+	ostream &operator<<(ostream &s, const DynamicArray &arr)
+	{
+		// [1 2 3 4 5 ]
+		for (unsigned int i = 0; i < arr.length; i++)
+		{
+			s << arr.data[i] << '\n'
+			  << endl;
+		}
+		s << endl;
+		return s;
+	}
 
 	// operator+ -> append a value to the array
 	// opertaor belongs to the class
@@ -66,6 +197,27 @@ private:
 	// data - array itself
 	ElementType* data;
 
-	void resize(unsigned int newCapacity);
+	void resize(unsigned int newCapacity)
+	{
+
+		this->capacity = newCapacity;
+		// no realloc  in c++
+
+		ElementType *newData = new ElementType[newCapacity]();
+		if (newData == nullptr)
+		{
+			cerr << "resize(): failed to allocate memory" << endl;
+			exit(-1);
+		}
+		// previous elements located in data
+		// copy them in newData
+		for (unsigned int i = 0; i < length; i++)
+		{
+			newData[i] = this->data[i];
+		}
+
+		delete[] this->data;
+		this->data = newData;
+	}
 };
 
